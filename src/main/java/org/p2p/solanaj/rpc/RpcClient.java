d936d4ae2044fff3e60e667ddc21f8e0722df74a
sk.kwon <skkwon80@gmail.com>
2022-01-26 09:27:28 +0900

    ProgramAccountConfig 생성자 추가

--- src/main/java/org/p2p/solanaj/rpc/RpcClient.java
+++ src/main/java/org/p2p/solanaj/rpc/RpcClient.java
@@ -1,25 +1,18 @@
 package org.p2p.solanaj.rpc;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
-
+import okhttp3.*;
 import org.p2p.solanaj.rpc.types.RpcRequest;
 import org.p2p.solanaj.rpc.types.RpcResponse;
 import org.p2p.solanaj.rpc.types.WeightedEndpoint;
 
-import javax.net.ssl.*;
+import javax.net.ssl.SSLHandshakeException;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class RpcClient {
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
@@ -33,15 +26,23 @@ public class RpcClient {
     }
 
     public RpcClient(Cluster endpoint) {
-        this(endpoint.getEndpoint());
+        this(endpoint.getEndpoint(), false);
     }
 
     public RpcClient(String endpoint) {
+        this(endpoint, false);
+    }
+
+    public RpcClient(String endpoint, boolean logging) {
         this.endpoint = endpoint;
-        this.httpClient = new OkHttpClient.Builder()
-                .readTimeout(20, TimeUnit.SECONDS)
-                //.addInterceptor(new LoggingInterceptor())
-                .build();
+
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder()
+                .readTimeout(20, TimeUnit.SECONDS);
+        if (logging) {
+            httpClientBuilder.addInterceptor(new LoggingInterceptor());
+        }
+        this.httpClient = httpClientBuilder.build();
+
         rpcApi = new RpcApi(this);
     }
 
@@ -89,6 +90,7 @@ public class RpcClient {
      * Returns RPC Endpoint based on a list of weighted endpoints
      * Weighted endpoints can be given a integer weight, with higher weights used more than lower weights
      * Total weights across all endpoints do not need to sum up to any specific number
+     *
      * @return String RPCEndpoint
      */
     private String getWeightedEndpoint() {
@@ -96,7 +98,7 @@ public class RpcClient {
         int randomMultiplier = cluster.endpoints.stream().mapToInt(WeightedEndpoint::getWeight).sum();
         double randomNumber = Math.random() * randomMultiplier;
         String currentEndpoint = "";
-        for (WeightedEndpoint endpoint: cluster.endpoints) {
+        for (WeightedEndpoint endpoint : cluster.endpoints) {
             if (randomNumber > currentNumber + endpoint.getWeight()) {
                 currentNumber += endpoint.getWeight();
             } else if (randomNumber >= currentNumber && randomNumber <= currentNumber + endpoint.getWeight()) {
