4c35ab8a045a61cb128e2ac53a714856309de9c7
sk.kwon <sk.kwon@battleent.com>
2022-02-11 15:32:40 +0900

    getSignatureStatuses reponse 데이터 추가

--- src/main/java/org/p2p/solanaj/rpc/types/SignatureStatuses.java
+++ src/main/java/org/p2p/solanaj/rpc/types/SignatureStatuses.java
@@ -12,12 +12,25 @@ public class SignatureStatuses extends RpcResultObject {
 
     @Getter
     @ToString
+    public static class Status {
+        @Json(name = "Ok")
+        private Object ok;
+    }
+
+    @Getter
+    @ToString
     public static class Value {
         @Json(name = "slot")
         private long slot;
 
         @Json(name = "confirmations")
         private Long confirmations;
+
+        @Json(name = "err")
+        private Object err;
+
+        @Json(name = "status")
+        private Status status;
 
         @Json(name = "confirmationStatus")
         private String confirmationStatus;
